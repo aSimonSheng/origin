79b132d6786a94089413d7f577b8d778c797b519
aSimonSheng <aSimonSheng@outlook.com>
2018-06-11 20:32:12 +0800

    创建一个db对象

--- manager.py
+++ manager.py
@@ -1,7 +1,18 @@
 # -*-coding:utf-8-*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@192.168.223.140/information'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app.config.from_object(Config)
+
+#创建db对象
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_word():
